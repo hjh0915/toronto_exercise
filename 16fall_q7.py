17641853fd6c5d32762ca98e1b3e4b86cb08e6d4
Passa <623299157@qq.com>
2018-08-11 21:48:32 +0800

    2016秋季练习题7

---
+++ 16fall_q7.py
@@ -0,0 +1,22 @@
+def find_occurrences(msg, sub):
+    """ (str, str) -> list of int
+
+    Return a list containing all of the indicies in msg where sub
+    appears.
+
+    >>> find_occurrences('Paul eats lollipops', 'lol')
+    [10]
+    >>> find_occurrences('lololol', 'lol')
+    [0, 2, 4]
+    """
+    result = []
+    occurrence = msg.find(sub)
+    while occurrence != -1:
+        result.append(occurrence)
+        occurrences = occurrence + 1
+        occurrence = msg.find(sub, occurrences)
+    return result
+
+if __name__ == '__main__':
+    print(find_occurrences('Paul eats lollipops', 'lol'))
+    print(find_occurrences('lololol', 'lol'))
